27874bb675f5e34b69c048869a4e89465a907534
sebastianterleira <seb.terleira1204@gmail.com>
2024-08-31 10:45:50 -0600

    feat: task familiarizarme con gsap.to()

---
+++ proyects/gsap-basics/index.js
@@ -0,0 +1,47 @@
+gsap.to("#box", {
+  opacity: 0.6,
+  scale: 0.5,
+  duration: 3,
+  rotation: 720,
+  delay: 1,
+  x: 1100,
+  y: -400,
+  background: "blue",
+  "border-radius": "50%",
+});
+
+gsap.to("#box2", {
+  opacity: 0.6,
+  scale: 0.5,
+  duration: 3,
+  rotation: 720,
+  delay: 1,
+  x: -1100,
+  y: 400,
+  background: "rgb(0, 140, 255)",
+  "border-radius": "50%",
+});
+
+gsap.to("#box3", {
+  opacity: 0.6,
+  scale: 0.5,
+  duration: 3,
+  rotation: 720,
+  delay: 1,
+  x: 1100,
+  y: 400,
+  background: "red",
+  "border-radius": "0%",
+});
+
+gsap.to("#box4", {
+  opacity: 0.6,
+  scale: 0.5,
+  duration: 3,
+  rotation: 720,
+  delay: 1,
+  x: -1100,
+  y: -400,
+  background: "#0f0",
+  "border-radius": "0%",
+});
